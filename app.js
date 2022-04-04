5402f1f2518f8050ed6ec249e1aa47b6452e6e8d
tree12132002 <tree12132002@hotmail.com>
2022-04-05 05:02:56 +0800

    add body parser

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// body parser
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.use(routes)
 
 // start and listen on the Express server
