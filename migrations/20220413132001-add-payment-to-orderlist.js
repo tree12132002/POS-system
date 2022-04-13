4e1d336e74827ebf6a495381faf16e6beb519689
tree12132002 <tree12132002@hotmail.com>
2022-04-13 21:22:27 +0800

    add payment column to orderlist model

---
+++ migrations/20220413132001-add-payment-to-orderlist.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Orderlists', 'payment', {
+      type: Sequelize.STRING,
+      allowNull: false
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Orderlists', 'payment')
+  }
+}
